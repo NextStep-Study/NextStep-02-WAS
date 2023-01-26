fd70dfb71315addf72989ea66a59755f0756be2a
dev-choee <rhksgh99@gmail.com>
2023-01-26 18:50:55 +0900

    gwanho TASK 5 : login using cookie

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.util.Map;
 
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,18 +41,23 @@ public class RequestHandler extends Thread {
             String[] tokens = line.split(" ");
             // HTTP 요청의 Content-Length 를 저장
             int contentLength = 0;
+            // 로그인 상태 정보
+            boolean logined = false;
 
             // HTTP Header 를 모두 읽을 때 까지 반복
             while (!line.equals("")){
                 line = br.readLine();
                 // HTTP header 부분 디버깅
                 log.debug("header : {}", line);
-                // 요청 본문에 Content-Length가 있으면 초기화
+                // 요청 헤더에 Content-Length가 있으면 초기화
                 if (line.contains("Content-Length")) contentLength = getContentLength(line);
+                // 요청 헤더에 Cookie 가 있으면 초기화
+                if (line.contains("Cookie")) logined = isLogin(line);
             }
 
-            // url 로 회원가입 분기처리
+            // url 로 분기처리
             String url = tokens[1];
+            // 회원가입
             if ("/user/create".equals(url)){
                 // 요청 본문의 길이 만큼 데이터를 읽어옴 - 필자가 구현해둔 IOUtils.readData() 활용
                 String body = IOUtils.readData(br, contentLength);
@@ -60,20 +66,53 @@ public class RequestHandler extends Thread {
                 // 사용자를 생성
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("User : {}", user);
+                // DB에 User 정보 저장
+                DataBase.addUser(user);
                 // 응답 상태 코드 302(redirect) 헤더, index.html로 redirect하도록 작성
                 DataOutputStream dos = new DataOutputStream(out);
-                response302Header(dos, "/index.html");
-            } else {
-                DataOutputStream dos = new DataOutputStream(out);
-                // file을 file io와 NIO로 처리
-                byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-                response200Header(dos, body.length);
-                responseBody(dos, body);
+                response302Header(dos);
             }
-
+            // 로그인
+            else if ("/user/login".equals(url)){
+                String body = IOUtils.readData(br, contentLength);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(body);
+                User user = DataBase.findUserById(params.get("userId"));
+                if (user == null) {
+                    responseResource(out, "/user/login_failed.html");
+                    return;
+                }
+                // 로그인 성공시 쿠키 저장 및 리다이렉트
+                if (user.getPassword().equals(params.get("password"))) {
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302LoginSuccessHeader(dos);
+                } else {
+                    responseResource(out, "/user/login_failed.html");
+                }
+            } else {
+                responseResource(out, url);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    // 로그인 여부 헤더의 쿠키에서 확인
+    private boolean isLogin(String line){
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) return false;
+        return Boolean.parseBoolean(value);
+    }
+
+    // url 경로 리소스 반환 메서드
+    private void responseResource(OutputStream out, String url) throws IOException{
+        DataOutputStream dos = new DataOutputStream(out);
+        // file을 file io와 NIO로 처리
+        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+
     }
 
     // 요청 본문에서 Content-Length 값을 추출하는 메서드
@@ -93,11 +132,23 @@ public class RequestHandler extends Thread {
         }
     }
 
-    // 응답 상태 코드 302(redirect) 헤더
-    private void response302Header(DataOutputStream dos, String url){
+    // 302(redirect) 리다이렉트 헤더
+    private void response302Header(DataOutputStream dos){
         try{
             dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
-            dos.writeBytes("Location: "+ url + " \r\n");
+            dos.writeBytes("Location: /index.html \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e){
+            log.error(e.getMessage());
+        }
+    }
+
+    // 로그인 성공시 쿠키 저장 헤더
+    private void response302LoginSuccessHeader(DataOutputStream dos){
+        try{
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Set-Cookie: logined=true \r\n");
+            dos.writeBytes("Location: /index.html \r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e){
             log.error(e.getMessage());
